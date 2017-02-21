f56d3c543997a4a7d9f063e14ba91e0142c76aa3
rana-abhi777 <abhisekrana777@gmail.com>
2017-02-21 09:04:12 +0530

    First commit of API Project

---
+++ APISignUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APISignUp
+//
+//  Created by Sierra 4 on 20/02/17.
+//  Copyright © 2017 code-brew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
